6a1dc58e984644bf66d4d7682d4f8f6588979e24
GitPit <rifflifter@gmail.com>
2023-02-22 11:21:10 -0500

    function LogoTitle()

--- LemonWelcome/navigators/RootNavigator.js
+++ LemonWelcome/navigators/RootNavigator.js
@@ -5,6 +5,16 @@ import SubscribeScreen from "../screens/SubscribeScreen";
 
 const Stack = createNativeStackNavigator();
 
+function LogoTitle() {
+    return <Image source={require('./assets/little-lemon-logo.png')} style={{
+      height: 40,
+      width: 300,
+      resizeMode: 'contain',
+      alignSelf: 'center',
+    }}
+     />;
+  }
+
 const RootNavigator = () => {
   return (
     <Stack.Navigator name="Welcome" component={WelcomeScreen} options={{ title: 'Home', headerTitle: (props) => <LogoTitle {...props} />,}}>
