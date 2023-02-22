9b473e4dd9fc64ae19f7a56b66551b22800d69e7
GitPit <rifflifter@gmail.com>
2023-02-22 11:37:06 -0500

    import RootNavigator from './navigators/RootNavig

--- LemonWelcome/App.js
+++ LemonWelcome/App.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import RootNavigator from './navigators/RootNavigator';
+import { Ionicons } from '@expo/vector-icons';
 
 const green = '#495E57';
 const yellow = '#F4CE14';
@@ -7,7 +9,7 @@ const yellow = '#F4CE14';
 export default function App() {
   return (
     <View style={styles.container}>
-      
+      <RootNavigator />
     </View>
   );
 }
