91d2d23cb5369ed02f9f6424b13a9f180448e990
GitPit <rifflifter@gmail.com>
2023-02-22 12:33:29 -0500

    import { useState } from 'react';

--- LemonWelcome/App.js
+++ LemonWelcome/App.js
@@ -1,16 +1,38 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import RootNavigator from './navigators/RootNavigator';
+import { NavigationContainer } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import WelcomeScreen from "../screens/WelcomeScreen";
+import SubscribeScreen from "../screens/SubscribeScreen";
 
 const green = '#495E57';
 const yellow = '#F4CE14';
 
+const Stack = createNativeStackNavigator();
+
+function LogoTitle() {
+    return <Image source={require('./assets/little-lemon-logo.png')} style={{
+      height: 40,
+      width: 300,
+      resizeMode: 'contain',
+      alignSelf: 'center',
+    }}
+     />;
+  }
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <RootNavigator />
+   <>
+   <NavigationContainer>
+   <View style={styles.container}>
+       <Stack.Navigator name="Welcome" component={WelcomeScreen} options={{ title: 'Home', headerTitle: (props) => <LogoTitle {...props} />,}}>
+     <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen name="Subscribe" component={SubscribeScreen} />
+    </Stack.Navigator>
     </View>
+   </NavigationContainer>
+   </>
   );
 }
 
