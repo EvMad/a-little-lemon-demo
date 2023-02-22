ff43f0b4b58fe7ccb3160cd078942e7d8474aff7
GitPit <rifflifter@gmail.com>
2023-02-22 12:38:59 -0500

    added styles to Subscribe screen

--- LemonWelcome/screens/SubscribeScreen.js
+++ LemonWelcome/screens/SubscribeScreen.js
@@ -34,4 +34,55 @@ const SubscribeScreen = (navigation) => {
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerText: {
+    padding: 40,
+    fontSize: 30,
+    color: '#EDEFEE',
+    textAlign: 'center',
+  },
+  regularText: {
+    fontSize: 24,
+    padding: 20,
+    marginVertical: 8,
+    color: '#EDEFEE',
+    textAlign: 'center',
+  },
+  input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+      fontSize: 16,
+      borderColor: '#EDEFEE',
+      backgroundColor: '#F4CE14',
+  },
+  button: {
+    fontSize: 22,
+    padding: 10,
+    marginVertical: 8,
+    margin: 40,
+    backgroundColor: '#EDEFEE',
+    borderColor: '#EDEFEE',
+    borderWidth: 2,
+    borderRadius: 12,
+},
+buttonText: {
+    color: '#333333',
+    textAlign: 'center',
+    fontSize: 32,
+},
+infoSection: {
+    fontSize: 24,
+    padding: 20,
+    marginVertical: 8,
+    color: '#EDEFEE',
+    textAlign: 'center',
+    backgroundColor: '#495E57',
+},
+});
+
 export default SubscribeScreen;
