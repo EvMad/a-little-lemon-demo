0993f81274be860e096b224fefd0fa2e5a758c7e
GitPit <rifflifter@gmail.com>
2023-02-22 11:43:29 -0500

    added style sheet to welcome screen

--- LemonWelcome/screens/WelcomeScreen.js
+++ LemonWelcome/screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Image, Pressable } from 'react-native';
+import { View, StyleSheet, Image, Pressable } from 'react-native';
 
 const WelcomeScreen = ({ navigation }) => {
   
@@ -11,5 +11,33 @@ const WelcomeScreen = ({ navigation }) => {
     </>
   );
 };
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,       
+  },
+  logo: {
+    height: 100,
+    width: 100,
+    borderRadius: 20,
+  },
+  button: {
+    fontSize: 22,
+    padding: 10,
+    marginVertical: 8,
+    margin: 40,
+    backgroundColor: '#EDEFEE',
+    borderColor: '#EDEFEE',
+    borderWidth: 2,
+    borderRadius: 12,
+},
+buttonText: {
+    color: '#333333',
+    textAlign: 'center',
+    fontSize: 32,
+},
+
+});
 
 export default WelcomeScreen;
