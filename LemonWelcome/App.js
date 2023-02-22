dd0a1bd2b49720ada4e166f5b4d8c84aa39bc4a5
GitPit <rifflifter@gmail.com>
2023-02-22 14:47:03 -0500

    stripped props from Stack.Navigator

--- LemonWelcome/App.js
+++ LemonWelcome/App.js
@@ -14,7 +14,7 @@ export default function App() {
    <>
    
 <NavigationContainer>
-
+<RootNavigator />
 </NavigationContainer>
    
    </>
--- LemonWelcome/navigators/RootNavigator.js
+++ LemonWelcome/navigators/RootNavigator.js
@@ -7,19 +7,9 @@ const Stack = createNativeStackNavigator();
 
 
 
-function LogoTitle() {
-    return <Image source={require('./assets/little-lemon-logo.png')} style={{
-      height: 40,
-      width: 300,
-      resizeMode: 'contain',
-      alignSelf: 'center',
-    }}
-     />;
-  }
-
 const RootNavigator = () => {
   return (
-    <Stack.Navigator name="Welcome" component={WelcomeScreen} options={{ title: 'Home', headerTitle: (props) => <LogoTitle {...props} />,}}>
+    <Stack.Navigator>
      <Stack.Screen name="Welcome" component={WelcomeScreen} />
       <Stack.Screen name="Subscribe" component={SubscribeScreen} />
     </Stack.Navigator>
