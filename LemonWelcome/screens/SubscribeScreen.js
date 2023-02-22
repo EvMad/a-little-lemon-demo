29843c996d7a83d39ca90d117adac6d526f328a6
GitPit <rifflifter@gmail.com>
2023-02-22 12:35:45 -0500

    const [email, onChangeEmail]

--- LemonWelcome/screens/SubscribeScreen.js
+++ LemonWelcome/screens/SubscribeScreen.js
@@ -3,6 +3,8 @@ import { useState } from 'react';
 import { ScrollView, Text, TextInput, StyleSheet, KeyboardAvoidingView, Platform, Pressable  } from 'react-native';
 
 const SubscribeScreen = (navigation) => {
+  const [showButton, setShowButton] = useState(false);
+    const [email, onChangeEmail] = React.useState('');
   // Add subscribe screen code here
   return <View></View>;
 };
