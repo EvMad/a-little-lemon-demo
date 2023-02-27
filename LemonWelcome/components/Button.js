d464212bdaff80f76d5366d2896356a33e46db67
GitPit <rifflifter@gmail.com>
2023-02-27 14:20:24 -0500

    added Pressable and Text to button.js

--- LemonWelcome/components/Button.js
+++ LemonWelcome/components/Button.js
@@ -2,7 +2,15 @@ import * as React from "react";
 import { Pressable, Text, StyleSheet } from "react-native";
 
 const Button = ({onPress, children, disabled}) => {
-    return ();
+    return (
+        <Pressable
+        onPress={onPress}
+        style={[styles.buttonWrapper, disabled && styles.disabled]}
+        disabled={disabled}
+      >
+        <Text style={styles.text}>{children}</Text>
+      </Pressable>
+    );
 };
 
 export default Button;
